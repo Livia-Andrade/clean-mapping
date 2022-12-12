41a002d8ab2f4413ed8fc8929741d60646f03284
Lívia Andrade <amethystsighs@icloud.com>
2022-12-12 16:18:09 -0300

    v.28

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,12 +30,12 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { matTooltipAnimations, MatTooltipModule } from '@angular/material/tooltip';
 
-
+//import for calendar
 import { CommonModule } from '@angular/common';
 // import { FlatpickrModule } from 'angularx-flatpickr';
-
-
-
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 //Componentes Pages
 
@@ -97,7 +97,17 @@ import { ContextComponent } from './pages/context/context.component';
     MatProgressBarModule,
     MatTooltipModule,
 
-    //import do sidebar
+    //import calendar
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    // FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+
+    //import sidebar
     MatToolbarModule,
     FormsModule,
     LayoutModule
