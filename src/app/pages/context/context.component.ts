4ff84ee1b5ceead826ad8db9662b51ec6b82e79a
Lívia Andrade <amethystsighs@icloud.com>
2022-12-12 17:12:20 -0300

    v29

--- src/app/pages/context/context.component.ts
+++ src/app/pages/context/context.component.ts
@@ -24,10 +24,28 @@ import {
 } from 'angular-calendar';
 import { EventColor } from 'calendar-utils';
 
+const colors: Record<string, EventColor> = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3',
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF',
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA',
+  },
+};
+
 @Component({
   selector: 'app-context',
   templateUrl: './context.component.html',
-  styleUrls: ['./context.component.scss']
+  styleUrls: ['./context.component.scss'
+
+
+]
 })
 export class ContextComponent {
 
