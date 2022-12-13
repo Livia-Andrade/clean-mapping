5b740fc6826015109a4020d140401f50e1b728a2
Kailaine Naiara <kailainenaiaraviana55422@gmail.com>
2022-12-13 17:16:00 -0300

    v31
    Coisas basicas

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+
+import { PeriodicElement } from '../pages/client/client.component';
+
+import { delay } from 'rxjs';
+
+
+
+@Injectable({
+
+  providedIn: 'root'
+
+})
+
+export class ClientService {
+
+  // private myurl = 'http://localhost:8080/client';
+
+  private readonly api = '/assets/client.json';
+  constructor(private httpClient: HttpClient) { }
+
+  listar() {
+    // return this.httpClient.get<PeriodicElement[]>(this.myurl).pipe(delay(2500));
+    return this.httpClient.get<PeriodicElement[]>(this.api).pipe(delay(2500));
+  }
+
+}
